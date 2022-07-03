912bbaf47ade933a9e44f25f5f252af5afc4970b
Carl Schroedl <carlschroedl@gmail.com>
2022-07-03 10:52:49 -0500

    Checkpoint

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Question
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions': latest_questions,
+    }
+    content = template.render(context, request)
+    response = HttpResponse(content)
+    return response
+
+def detail(request, question_id):
+    response = f"You're viewing question {question_id}"
+    return HttpResponse(response)
+
+def results(request, question_id):
+    response = f"You're view the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    response = f"You're voting on question {question_id}"
+    return HttpResponse(response)
