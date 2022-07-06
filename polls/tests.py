8022a49f28da8852dc1f9fa23c5afe2ee02511ef
Carl Schroedl <carlschroedl@gmail.com>
2022-07-05 20:34:15 -0500

    View tests

--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,29 @@ from django.utils import timezone
 
 from .models import Question
 
+from django.urls import reverse
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed
+        """
+
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_questions'], [])
+
 class QuestiomModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -24,7 +47,7 @@ class QuestiomModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
-        
+
     def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() returns True for questions whose pub_date
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -11,7 +12,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_questions'
 
     def get_queryset(self):
-        return Question.objects.order_by('pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+        ).order_by('pub_date')[:5]
     
 # Should this be renamed to "QuestionView?"
 class DetailView(generic.DetailView):
